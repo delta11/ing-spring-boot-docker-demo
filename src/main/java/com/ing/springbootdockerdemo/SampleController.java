7862b058702636259f48a62cf9dc58853972bdb1
workshop <thomasvanputten@hotmail.com>
2016-08-12 12:59:54 +0200

    Step2 just some basic spring stuff added to the endpoint

--- src/main/java/com/ing/springbootdockerdemo/SampleController.java
+++ src/main/java/com/ing/springbootdockerdemo/SampleController.java
@@ -5,15 +5,27 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
+    //This is of course not the way to do this but this is just a quick and dirty example
+    private ArrayList<Todo> todos = new ArrayList<Todo>();
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-        return "Hello Basic app!";
+    List<Todo> home() {
+        return todos;
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    Todo home(@RequestBody Todo input) {
+        todos.add(input);
+        return input;
+    };
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
---
+++ src/main/java/com/ing/springbootdockerdemo/Todo.java
@@ -0,0 +1,22 @@
+package com.ing.springbootdockerdemo;
+
+public class Todo {
+    private String name;
+
+    public Todo(){
+    }
+
+    public Todo(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
